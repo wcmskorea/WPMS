dc5feaeefb572e96c1adbaca80e2824e4450485f
Sung Joon, Lee <wcmskorea@gmail.com>
2018-06-01 18:23:27 +0900

    todos schema upate

---
+++ database/migrations/TodosMigration.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTodosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('todos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+
+        Schema::table('todos', function($table){
+            $table->tinyInteger('done');
+        });
+
+        Schema::table('todos', function($table){
+            $table->tinyInteger('progress');
+        });
+
+        Schema::table('todos', function($table){
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('todos');
+    }
+}
